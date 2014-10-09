efdd6bb5299e2ecb6ba97556e7f0bce0dc6bab71
Cameron Shellum <shellum@gmail.com>
2014-10-09 17:59:09 -0600

    Start styling

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,7 +14,7 @@ object Application extends Controller {
   val FAMILYSEARCH_SERVER_URL: String = Play.current.configuration.getString("familysearch.server.url").get
 
   def index = Action {
-    Ok(views.html.index(""))
+    Ok(views.html.index())
   }
 
   def processToken = Action { implicit request =>
