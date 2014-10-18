6e7b1fd90e0eedcecb43e57e0e0cf592cc034bf1
Cameron Shellum <shellum@gmail.com>
2014-10-17 23:11:26 -0600

    Use None instead of null/reformat

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,16 @@
 package controllers
 
+import models.Person
 import play.api.Play
+import play.api.data.Forms._
+import play.api.data._
 import play.api.libs.json.{JsObject, Json}
 import play.api.libs.ws.WS
 import play.api.mvc._
-import models.Person
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
-import ExecutionContext.Implicits.global
-import play.api.data.Forms._
-import play.api.data._
-import play.api.mvc._
-import models.Person
 
 object Application extends Controller {
 
@@ -34,29 +33,29 @@ object Application extends Controller {
       "code" -> Seq(code),
       "client_id" -> Seq(FAMILYSEARCH_DEVELOPER_ID))
     val future = WS.url(FAMILYSEARCH_SERVER_URL + "/cis-web/oauth2/v3/token")
-    .post(requestMap).map { response =>
+      .post(requestMap).map { response =>
       ret = response.body
     }
 
-    Await.result(future,Duration(5, java.util.concurrent.TimeUnit.SECONDS))
+    Await.result(future, Duration(5, java.util.concurrent.TimeUnit.SECONDS))
     val json = Json.parse(ret)
     var token: String = (json \ "access_token").toString
-    token = "Bearer " + token.replaceAll("\"","")
+    token = "Bearer " + token.replaceAll("\"", "")
 
     val user = getCurrentUser(token)
     val j = Json.parse(user)
     val jsarray = j \ "users"
-    val personId = (jsarray(0) \ "personId").toString().replace("\"","")
-    val displayName = (jsarray(0) \ "displayName").toString().replace("\"","")
+    val personId = (jsarray(0) \ "personId").toString().replace("\"", "")
+    val displayName = (jsarray(0) \ "displayName").toString().replace("\"", "")
     Ok(views.html.menu(token, personId))
   }
 
   def getCurrentUser(token: String) = {
     var ret = ""
     val future = WS.url(FAMILYSEARCH_SERVER_URL + "/platform/users/current")
-      .withHeaders(("Accept","application/x-fs-v1+json"),("Authorization",token))
+      .withHeaders(("Accept", "application/x-fs-v1+json"), ("Authorization", token))
       .get().map { response =>
-        ret = response.body
+      ret = response.body
     }
 
     Await.result(future, Duration(25, java.util.concurrent.TimeUnit.SECONDS))
@@ -68,29 +67,29 @@ object Application extends Controller {
     val pid = userForm.bindFromRequest.get.pid
 
     var addedCousins: Map[String, Person] = Map[String, Person]()
-    val grandparentSet = Person("Grandparent Set", "", "",null, false)
+    val grandparentSet = Person("Grandparent Set", "", "", None, false)
 
     //Walk up the tree
-    val parents:List[Person] = getParents(token, Person("doesn't matter",pid, "",null, false))
-    val grandparents = parents.foldLeft(List[Person]())((acc, item)=> getParents(token, item) ::: acc)
+    val parents: List[Person] = getParents(token, Person("doesn't matter", pid, "", None))
+    val grandparents = parents.foldLeft(List[Person]())((acc, item) => getParents(token, item) ::: acc)
 
     //Walk back down the tree
-    grandparents.foreach((item)=>{
+    grandparents.foreach((item) => {
       val children = getChildren(token, pid, item)
       grandparentSet.addDecendents(children)
     })
 
     //auntsUncles = auntsUncles diff parents
-    val cousins = grandparentSet.getDecendents().foldLeft(List[Person]())((acc, item)=>{
+    val cousins = grandparentSet.getDecendents().foldLeft(List[Person]())((acc, item) => {
       var children = getChildren(token, pid, item)
       var toRemove = List[Person]()
-      children.foreach((child)=> {
-        val addedCousin = addedCousins.get(child.pid).getOrElse(Person("","","",null,false))
+      children.foreach((child) => {
+        val addedCousin = addedCousins.get(child.pid).getOrElse(Person("", "", "", None))
         if (addedCousin.getPid == "") {
           addedCousins += (child.getPid() -> child)
         }
         else {
-          addedCousin.parent.altName =  " & " + item.name
+          addedCousin.parent.get.altName = " & " + item.name
           toRemove = child :: toRemove
         }
 
@@ -100,7 +99,7 @@ object Application extends Controller {
       children ::: acc
     })
 
-    var cousinList = cousins.foldLeft(List[Person]())((acc, item)=>{
+    var cousinList = cousins.foldLeft(List[Person]())((acc, item) => {
       if (item.pid != pid)
         item :: acc
       else
@@ -109,7 +108,7 @@ object Application extends Controller {
 
     cousinList = cousinList.sortBy(_.getName())
 
-    val gps = Person("All Grandparents","","",null,false)
+    val gps = Person("All Grandparents", "", "", None)
     gps.addDecendents(grandparentSet.getDecendents())
     val json = gps.toJson
 
@@ -117,39 +116,38 @@ object Application extends Controller {
   }
 
   def getParents(token: String, person: Person): List[Person] = {
-    var ret:List[Person] = List[Person]()
-    val future = WS.url(FAMILYSEARCH_SERVER_URL + "/platform/tree/persons/"+person.getPid()+"/parents")
-      .withHeaders(("Accept","application/x-fs-v1+json"),("Authorization",token))
+    var ret: List[Person] = List[Person]()
+    val future = WS.url(FAMILYSEARCH_SERVER_URL + "/platform/tree/persons/" + person.getPid() + "/parents")
+      .withHeaders(("Accept", "application/x-fs-v1+json"), ("Authorization", token))
       .get().map { response =>
       val body = response.body
       val json = Json.parse(body)
       val jsarray = json \ "persons"
-      ret = jsarray.as[List[JsObject]].foldLeft(List[Person]())((acc, item)=>{
-        val id = (item \ "id").toString().replaceAll("\"","")
-        val name = (item \ "display" \ "name").toString().replaceAll("\"","")
-        val gender = (item \ "gender" \ "type").toString().replaceAll("\"","").replace("http://gedcomx.org/","")
-        Person(name, id, gender, null, false) :: acc
+      ret = jsarray.as[List[JsObject]].foldLeft(List[Person]())((acc, item) => {
+        val id = (item \ "id").toString().replaceAll("\"", "")
+        val name = (item \ "display" \ "name").toString().replaceAll("\"", "")
+        val gender = (item \ "gender" \ "type").toString().replaceAll("\"", "").replace("http://gedcomx.org/", "")
+        Person(name, id, gender, None) :: acc
       })
     }
     Await.result(future, Duration(50, java.util.concurrent.TimeUnit.SECONDS))
     ret
   }
+
   def getChildren(token: String, selfPid: String, parent: Person): List[Person] = {
-    var ret:List[Person] = List[Person]()
-    val future = WS.url(FAMILYSEARCH_SERVER_URL + "/platform/tree/persons/"+parent.getPid+"/children")
-      .withHeaders(("Accept","application/x-fs-v1+json"),("Authorization",token))
+    var ret: List[Person] = List[Person]()
+    val future = WS.url(FAMILYSEARCH_SERVER_URL + "/platform/tree/persons/" + parent.getPid + "/children")
+      .withHeaders(("Accept", "application/x-fs-v1+json"), ("Authorization", token))
       .get().map { response =>
       val body = response.body
       val json = Json.parse(body)
       val jsarray = json \ "persons"
-      ret = jsarray.as[List[JsObject]].foldLeft(List[Person]())((acc: List[Person], item: JsObject)=>{
-        val id = (item \ "id").toString().replaceAll("\"","")
-        val name = (item \ "display" \ "name").toString().replaceAll("\"","")
-        val gender = (item \ "gender" \ "type").toString().replaceAll("\"","").replace("http://gedcomx.org/","")
-        var highlight = false
-        if (id.equals(selfPid))
-          highlight = true
-        Person(name,id, gender, parent, highlight) :: acc
+      ret = jsarray.as[List[JsObject]].foldLeft(List[Person]())((acc: List[Person], item: JsObject) => {
+        val id = (item \ "id").toString().replaceAll("\"", "")
+        val name = (item \ "display" \ "name").toString().replaceAll("\"", "")
+        val gender = (item \ "gender" \ "type").toString().replaceAll("\"", "").replace("http://gedcomx.org/", "")
+        val highlight = id.equals(selfPid)
+        Person(name, id, gender, Some[Person](parent), highlight) :: acc
       })
     }
     Await.result(future, Duration(50, java.util.concurrent.TimeUnit.SECONDS))
--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,63 +1,36 @@
 package models
 
-import play.api.libs.json.{JsPath, Writes, Json}
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-
-case class Person(name: String, pid: String, gender: String, parent: Person, highlight: Boolean) {
+case class Person(name: String, pid: String, gender: String, parent: Option[Person], highlight: Boolean = false) {
   var children: List[Person] = List[Person]()
   var altName = ""
-  def addDecendent(person: Person) = {
-    var alreadyExists = false
-    System.out.println("trying to add "+person.name)
-    children.foreach((child)=> {
-      System.out.println("    comparing "+child.pid+" to " + person.pid + " with " + child.name + " to " + person.name)
-      if (child.pid == person.pid)
-        alreadyExists = true
-    })
-    System.out.println("    already there=="+alreadyExists)
 
-    if (!alreadyExists)
+  override def hashCode(): Int = super.hashCode()
+
+  override def equals(p1: scala.Any): Boolean = p1.asInstanceOf[Person].pid == pid
+
+  def getName() = name
+
+  def getPid() = pid
+
+  def getDecendents() = children
+
+  def addDecendent(person: Person) = {
+    if (!children.contains(person))
       children = person :: children
   }
+
   def addDecendents(people: List[Person]) = {
-    people.foreach((person)=> {
+    people.foreach((person) => {
       addDecendent(person)
     })
   }
-  def getDecendents() = children
 
-
-  override def equals(p1: scala.Any): Boolean = p1.asInstanceOf[Person].getPid().equals(pid)
-
-  override def hashCode = {
-    var ret: Int = 0
-    pid.foreach((c)=> {
-      ret * 1000 + c.toInt
-    })
-    ret
-  }
-  def getName() = name
-  def getPid() = pid
-
-  def toJson : String = {
-    var ret = "{\"name\":\""+name+altName+"\", \"pid\":\""+pid+"\", \"gender\":\""+gender+"\", \"highlight\":"+highlight+", \"children\":[ "
-    if (children != null)
-      children.foreach((p)=> ret += p.toJson + ",")
+  def toJson: String = {
+    var ret = "{\"name\":\"" + name + altName + "\", \"pid\":\"" + pid + "\", \"gender\":\"" + gender + "\", \"highlight\":" + highlight + ", \"children\":[ "
+    if (children.size > 0)
+      children.foreach((p) => ret += p.toJson + ",")
     ret += "]}"
     ret
   }
 
 }
-/*
-object Person {
-  //implicit val PersonWrites = Json.writes[Person]
-
-  implicit val PersonWrites: Writes[Person] = (
-    (__ \ "name").write[String] and
-    (__ \ "pid").write[String] and
-    (__ \ "children").lazyWrite(Writes.traversableWrites[Person](PersonWrites))
-    )(unlift(Person.unapply))
-
-  def unapply(person:Person): Option[(String, String, List[Person])] = Some((person.getName, person.getPid, person.getDecendents()))
-}*/
